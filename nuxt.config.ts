4414c85eee2a3f3f225acee99f409a700e651a6f
Veverre <hrv.leroy@gmail.com>
2025-08-05 11:17:07 +0200

    feat: install tailwind

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,10 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from '@tailwindcss/vite'
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint']
+  modules: ['@nuxt/eslint'],
+   vite: {
+    plugins: [tailwindcss()],
+  },
+  css: ['~/assets/styles/main.css'],
 })
