d117783eabe046a357b0573e4af5872df3487f26
Veverre <hrv.leroy@gmail.com>
2025-08-05 12:18:21 +0200

    feat: init cloudflare

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,14 @@
-import tailwindcss from '@tailwindcss/vite'
+import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
+  compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint'],
-   vite: {
+  modules: ["@nuxt/eslint"],
+  vite: {
     plugins: [tailwindcss()],
   },
-  css: ['~/assets/styles/main.css'],
-})
+  css: ["~/assets/styles/main.css"],
+  ssr: true,
+  nitro: {
+    preset: "cloudflare",
+  },
+});
